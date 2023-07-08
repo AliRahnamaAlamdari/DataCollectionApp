a6444fcdcdab43aee9559852098794e719f530c6
mohammad amin geramimehr <59396870+MohammadAmin-2001@users.noreply.github.com>
2023-07-08 11:58:34 +0330

    add device info feat

--- androidApp/src/main/java/com/example/datacollectionapplication/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/datacollectionapplication/android/MainActivity.kt
@@ -1,40 +1,42 @@
-package com.example.datacollectionapplication.android
-
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.datacollectionapplication.Greeting
+import android.provider.Settings
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
-            }
-        }
-    }
-}
+class MainActivity : AppCompatActivity() {
 
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
 
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
-    }
+		val tvDisplay: TextView = findViewById(R.id.tv_displayInfo)
+		tvDisplay.text = getSystemDetail()
+	}
+
+	@SuppressLint("HardwareIds")
+	private fun getSystemDetail(): String {
+		return "Brand: ${Build.BRAND} \n" +
+				"DeviceID: ${
+					Settings.Secure.getString(
+						contentResolver,
+						Settings.Secure.ANDROID_ID
+					)
+				} \n" +
+				"Model: ${Build.MODEL} \n" +
+				"ID: ${Build.ID} \n" +
+				"SDK: ${Build.VERSION.SDK_INT} \n" +
+				"Manufacture: ${Build.MANUFACTURER} \n" +
+				"Brand: ${Build.BRAND} \n" +
+				"User: ${Build.USER} \n" +
+				"Type: ${Build.TYPE} \n" +
+				"Base: ${Build.VERSION_CODES.BASE} \n" +
+				"Incremental: ${Build.VERSION.INCREMENTAL} \n" +
+				"Board: ${Build.BOARD} \n" +
+				"Host: ${Build.HOST} \n" +
+				"FingerPrint: ${Build.FINGERPRINT} \n" +
+				"Version Code: ${Build.VERSION.RELEASE}"
+	}
+
 }
